6f41c35d0000fbcc46ae5d9208a91e7dd214a821
AndersonBillss <anderson@thebillsfamily.com>
2024-10-18 10:33:14 -0600

    added some more components

--- src/GUI/GUI.cs
+++ src/GUI/GUI.cs
@@ -10,15 +10,37 @@ public partial class GUI : Form
         {
             Text = "Click Me",
             Location = new Point(),
-            Padding = new Padding{
-                
-            },
+            Padding = new Padding(10,10,10,10),
             BackColor = Color.MediumAquamarine,
         };
         myButton.Click += new EventHandler(MyButton_Click); // Event handler for button click
+        Label myLabel = new Label
+        {
+            Text = "Enter your name:",
+            Location = new Point(50, 120),
+            AutoSize = true,
+        };
+
+        // Create a new textbox
+        TextBox myTextBox = new TextBox
+        {
+            Location = new Point(50, 150),
+            Width = 200,
+        };
+
+        // Create a new checkbox
+        CheckBox myCheckBox = new CheckBox
+        {
+            Text = "I agree to the terms and conditions",
+            Location = new Point(50, 200),
+            AutoSize = true,
+        };
 
         // Add the button to the form
         Controls.Add(myButton);
+        Controls.Add(myLabel);
+        Controls.Add(myTextBox);
+        Controls.Add(myCheckBox);
         InitializeComponent();
     }
 
